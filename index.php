f255a32da1b3d85bdcb4791e2796b8def563b820
maparrar <maparrar@gmail.com>
2015-10-14 16:18:30 -0500

    Primera versión de borrado de imagen

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?>
 <html>
-    
     <head>
+        <title>yo-objeto</title>
+
+        <!-- Se incluyen los estilos -->
+        <link rel="stylesheet" type="text/css" href="css/general.css">
         
+        <!-- Se incluyen los scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="js/eraser/jquery.eraser.js"></script>
+        
+        <script>
+            $( document ).ready(function() {
+                $('#top').eraser();
+            });
+        </script>
     </head>
     <body>
-        <h1>Hola mi chinita</h1>
-        <h2>Estamos en línea!!!</h2>
+        <h1>Yo-objeto</h1>        
+        <div id="images">
+            <img id="bottom" class="image" src="images/like.jpg" />
+            <img id="top" class="image" src="images/robot.jpg" />
+        </div>
     </body>
 </html>
