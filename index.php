5d0b5d622a7663ec9918a762a3e6311430536ffa
maparrar <maparrar@gmail.com>
2015-10-14 15:46:31 -0500

    Prueba de index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<html>
+    
+    <head>
+        
+    </head>
+    <body>
+        <h1>Hola mi chinita</h1>
+        <h2>Estamos en línea!!!</h2>
+    </body>
+</html>
